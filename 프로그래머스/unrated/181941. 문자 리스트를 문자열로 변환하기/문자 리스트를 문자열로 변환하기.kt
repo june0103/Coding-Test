39507e72781703ad1e365cd382a91bdcead14b53
June <66195205+june0103@users.noreply.github.com>
2023-06-10 21:45:09 +0900

    [unrated] Title: 문자 리스트를 문자열로 변환하기, Time: 2.26 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(arr: Array<String>): String {
+        var answer: String = ""
+        for(char in arr){
+            answer += char
+        }
+        return answer
+    }
+}
